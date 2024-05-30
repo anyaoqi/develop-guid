807b8eb278784c2216051be158c73c8a9431098d
杨奇 <yangq1@hsit.com.cn>
2024-05-30 17:52:59 +0800

    创建前端开发规范项目

---
+++ .vitepress/config.js
@@ -0,0 +1,21 @@
+// https://vitepress.dev/zh/reference/site-config
+export default {
+  // 站点级选项
+  title: '前端开发规范',
+  description: '',
+  srcDir: 'src',
+  themeConfig: {
+    logo: '/logo.ico',
+    // 导航
+    nav: [
+      { text: '首页', link: '/index' },
+      { text: '代码规范', link: '/docs/code' },
+      { text: '开发规范', link: '/docs/dev' }
+    ],
+    outline: {
+      label: '文章导航'
+    },
+  },
+  rewrites: {
+  }
+}
