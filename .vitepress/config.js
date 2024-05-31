f8ef629c1d29fb832c9181c62e508e8a7b73cb70
杨奇 <yangq1@hsit.com.cn>
2024-05-31 09:50:44 +0800

    修改base

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -1,7 +1,7 @@
 // https://vitepress.dev/zh/reference/site-config
 export default {
   // 站点级选项
-  base: './',
+  base: './src',
   title: '前端开发规范',
   description: '',
   srcDir: 'src',
@@ -10,8 +10,8 @@ export default {
     // 导航
     nav: [
       { text: '首页', link: '/index' },
-      { text: '代码规范', link: '/docs/code/index' },
-      { text: '开发规范', link: '/docs/dev/index' }
+      { text: '代码规范', link: '/docs/code' },
+      { text: '开发规范', link: '/docs/dev' }
     ],
     outline: {
       label: '文章导航'
